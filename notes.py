f2cb80cc5ff308bc4c804d968d71ab8a991205a8
John McAree <cowboyslyrics@gmail.com>
2022-02-03 20:24:14 +0000

    First Commit
    First Commit

---
+++ notes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+https://www.decodingdevops.com/python-write-to-file-line-by-line/
+write to the pokerstars notes.xml
+use the above link as a tutorial
+
+"""
+index=1627255667
+donors= "<note player=\"Artishell\" label=\"0\" update=\"" + str(index) + "\"></note>\n"
+player = "Artishell"
+
+F = open("note.xml","a")
+
+for line in range(5):
+    F.writelines(donors)
+    index  +=1
+    donors= "<note player=\"Artishell\" label=\"0\" update=\"" + str(index) + "\"></note>\n"
+    
+
+F.close()
